d166228339241fae0bf1ca60efaf2082a40875d5
Francisco Prin <franciscoprin@gmail.com>
2023-07-01 15:42:14 -0600

    Setting up multiple networking elements: 1) Modifying the default VPC's security group to protect all the instances inside it. 2) Creating an internet gateway to allow bidirectional communication between the instances in the VPC and the internet. 3) Creating a route table for the VPC. 4) Adding a route in the route table to the internet gateway to enable bidirectional communication with the internet. 5) Creating a network load balancer to allow the Control Plane node to manage the worker clusters.

--- terraform/kubernetes-cluster/vpc.tf
+++ terraform/kubernetes-cluster/vpc.tf
@@ -19,3 +19,128 @@ resource "aws_subnet" "k8s_subnet" {
     Name = "kubernetes-the-hard-way"
   }
 }
+
+# Default security rules for the VPC
+#  ( It is a firewall for all the resources inside of the VPC )
+resource "aws_security_group" "k8s_sg" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  # All protocols are allowed for the specified private CIDR blocks
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.1.0.0/16", "10.201.0.0/16"]
+  }
+
+  # TCP protocol is allowed for the specified ports
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ICMP protocol is allowed for all IPs in all the Ports.
+  #  for network troubleshooting and diagnostic purposes, 
+  #  such as ping requests.
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an internet gateway for bidirectional communication
+# between the k8s EC2 cluster in subnet and the internet.
+resource "aws_internet_gateway" "k8s_igw" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  tags = {
+    Name = "kubernetes"
+  }
+}
+
+# Create a route table that will control the traffic flow of the k8s EC2 cluster.
+resource "aws_route_table" "k8s_route_table" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  tags = {
+    Name = "kubernetes"
+  }
+}
+
+# Create a route for the internet gateway
+# Allow all the instances in the subnet to communicate with the internet.
+resource "aws_route" "k8s_internet_gateway_route" {
+  route_table_id         = aws_route_table.k8s_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.k8s_igw.id
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "k8s_route_table_association" {
+  subnet_id      = aws_subnet.k8s_subnet.id
+  route_table_id = aws_route_table.k8s_route_table.id
+}
+
+# Create a load balancer in the same subnet where the K8S EC2 cluster will be created:
+resource "aws_lb" "lb_k8s" {
+  name               = "kubernetes"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = [aws_subnet.k8s_subnet.id]
+
+  tags = {
+    Name = "kubernetes"
+  }
+}
+
+# Create a target group to which the load balancer will balance traffic:
+resource "aws_lb_target_group" "target_group_k8s" {
+  name        = "kubernetes"
+  port        = 6443
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.k8s_vpc.id
+  target_type = "ip"
+
+  health_check {
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Attach worker nodes EC2 instances to the target group:
+resource "aws_lb_target_group_attachment" "kubernetes" {
+  count            = 3
+  target_group_arn = aws_lb_target_group.target_group_k8s.arn
+  target_id        = "10.1.1.1${count.index}"
+  port             = 6443
+}
+
+# Specify the port and protocol on which the load balancer will listen to redirect traffic to the workers nodes:
+resource "aws_lb_listener" "kubernetes" {
+  load_balancer_arn = aws_lb.lb_k8s.arn
+  protocol          = "TCP"
+  port              = "433"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.target_group_k8s.arn
+    type             = "forward"
+  }
+}
